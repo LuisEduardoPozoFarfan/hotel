f72a4f9b8e27a3300fe0a608f00b0a5486f812ed
ovalcorp <luis.pozo.f@uni.pe>
2024-10-24 16:28:01 -0500

    actulizado el sistema de rutas

--- hotel_react/src/App.js
+++ hotel_react/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react' 
-import "./App.scss"
+import { Navigation } from "./routes"
 
 export default function App() { 
   return ( 
    <div> 
-     <h1>Hola Mundo de React</h1> 
+     <Navigation/>
    </div> 
  ) 
 } 
