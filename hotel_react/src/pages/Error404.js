ffb126cdc549382c3ff39d13fab5f167fb97138b
luisEduardoPozoFarfan <pozofarfanluis20@gmail.com>
2024-10-24 20:24:29 -0500

    rutas validad con error 404

--- hotel_react/src/pages/Error404.js
+++ hotel_react/src/pages/Error404.js
@@ -1,9 +1,16 @@
 import React from "react"
+import "./Error404.scss"
 
 export function Error404() {
     return (
-        <div>
-            <p>Error404</p>
+        <div class="container">
+            <h1 class="error-code">404</h1>
+            <h2 class="error-message">¡Oh no! Página no encontrada</h2>
+            <p class="info">
+                Parece que la página que estás buscando no está disponible. 
+                Pero no te preocupes, ¡aún podemos ayudarte a encontrar lo que necesitas!
+            </p>
+            <a href="/" class="btn">Volver a la página principal</a>
         </div>
     )
 }
